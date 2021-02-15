4b4cd1882e7eb02b52eefe89a5935cec468ff678
Akberto Tienda <albertotienda1@outlook.com>
2021-02-15 14:17:13 -0600

    clase3

---
+++ variables.js
@@ -0,0 +1,5 @@
+let primerCommit = 'Este es el inicio de mi repo';
+console.log(primerCommit);
+
+let miCommitColaborativo = 'Soy el colaborador de este repositorio';
+console.log(miCommitColaborativo); 
